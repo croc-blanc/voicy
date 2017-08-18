fa029bce3007d76d857f3f00b5e4c06affc7ebf1
valentin fage <valentin-fage@vfage.com>
2017-08-18 18:43:42 +0200

    page edit complete avec upload photo/voices/videos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.new
+    @user = User.find(params[:id])
   end
 
   def show
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to "/"
+    else
+      render :edit
+    end
   end
+
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :photos)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
                  :recoverable, :rememberable, :trackable, :validatable
   has_many       :bookings
   has_many       :projects
-  has_attachment :photos
-  has_attachment :videos
-  has_attachment :voices
+  has_attachments :photos, maximum: 10 #limiting upload to 10 units
+  has_attachments :videos, maximum: 10 #limiting upload to 10 units
+  has_attachments :voices, maximum: 10 #limiting upload to 10 units
+
+  enum voice_attribute: [:grave, :moyen, :aigu]
+  enum gender: [:femme, :homme]
+  enum avaibility: [:oui, :non]
 end
