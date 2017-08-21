a7529ea2adcbef6c1931a5d3b20a526839082ab4
David MARIE <eth3rnit3@outlook.fr>
2017-08-21 12:36:39 +0200

    As an da i can create show and edit a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,12 +5,17 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    @project = Project.new
+    if current_user.role == "artistic_director" # A modifier par greg
+        @project = Project.new
+    else
+      flash[:notice] = "Your should be Artistic director to create a project "
+      redirect_to root_path
+    end
   end
 
   def create
-    @project = Project.new(project_params)
-    @project.user_id = current_user.id
+  @project = Project.new(project_params)
+  @project.user_id = current_user.id
     if @project.save
       flash[:notice] = "Your project has been created"
       redirect_to project_path(@project)
@@ -20,6 +25,10 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+    if current_user.role != "artistic_director" # A modifier par greg
+      flash[:notice] = "Your should be Artistic director to edit a project "
+      redirect_to root_path
+    end
   end
 
   def show
@@ -45,7 +54,5 @@ class ProjectsController < ApplicationController
 
   def project_params
     params.require(:project).permit(:name, :description, :category, :begin, :end)
-    # petit probleme, le save se passe bien mais la category n'est pas enregister
-    # car elle n'est meme pas envoyer dans les params lors du create
   end
 end
