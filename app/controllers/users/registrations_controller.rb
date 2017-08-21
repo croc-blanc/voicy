ba02e398bb3e23f6c586f8aa4b1606cddbf50032
gregoire dupouy <gregoire.dupouy@gmail.com>
2017-08-21 18:01:06 +0200

    user can edit his password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,7 +51,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    "/users/:id"
+    "/users/#{current_user.id}/edit"
   end
 
   def role_to_integer
