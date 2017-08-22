2d5c95b758f88a43af9d76bb482e6e3801a85545
valentin fage <valentin-fage@vfage.com>
2017-08-22 14:51:15 +0200

    user index css done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    if current_user.DA? # A modifier par greg
+    if current_user.actor? # A modifier par greg
         @project = Project.new
     else
       flash[:notice] = "Your should be Artistic director to create a project "
@@ -25,7 +25,7 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    if current_user.DA?
+    if current_user.actor?
       flash[:notice] = "Your should be Artistic director to edit a project "
       redirect_to root_path
     end
