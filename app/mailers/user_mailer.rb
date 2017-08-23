815a95532769a11a3a24668e2e0cadc5c66f42e6
valentin fage <valentin-fage@vfage.com>
2017-08-23 15:35:22 +0200

    email en production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < ApplicationMailer
+
   def welcome(user)
     @user = user  # Instance variable => available in view
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,14 @@
 class Booking < ApplicationRecord
+
+  after_create :send_booking_email
+
   belongs_to :project
   belongs_to :user
   validates :user_id, uniqueness: { scope: :project }
+
+private
+
+  def send_booking_email
+    BookingMailer.bookingproject(self).deliver_now
+  end
 end
